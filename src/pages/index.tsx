6dc567b7ec1a599aebf2583631864601b747440e
tiagobw <79862444+tiagobw@users.noreply.github.com>
2021-03-30 06:17:14 -0300

    Update index.tsx
    correct home revalidate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -144,6 +144,6 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {
       postsPagination,
     },
-    redirect: 60 * 60 * 24, // 24 hours
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
